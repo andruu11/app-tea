b3c12a06a624dd834ffd219540bc2bb6b4939a82
Jhassir Candia <andruu11.jhass@gmail.com>
2018-10-19 18:05:43 -0400

    Pagina Inicial

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="vista/assets/css/main-page.css" rel="stylesheet">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+    <div id="custom-bootstrap-menu" class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header"><img src="vista/assets/img/logoteas.svg" alt="" height="50px">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse navbar-menubuilder">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="index.php">Inicio</a></li>
+        <li class="dropdown">
+          <a href="quienes_somos.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Quienes Somos<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="quienes_somos.php">Misión</a></li>
+            <li><a href="quienes_somos.php">Visión</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="tea.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">TEA<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="tea.php">Historia</a></li>
+            <li><a href="tea.php">Evolución</a></li>
+          </ul>
+        </li>
+        <li><a href="contactos.php">Contactos</a></li>
+        <li><a href="login.php">Iniciar Sesión</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="row slide">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="vista/assets/img/op1.jpg" claas="img-responsive" alt="">
+    </div>
+
+    <div class="item">
+      <img src="vista/assets/img/op2.jpg" claas="img-responsive" alt="">
+    </div>
+
+    <div class="item">
+      <img src="vista/assets/img/op3.jpg" claas="img-responsive" alt="">
+    </div>
+  </div>
+</div>
+</div>
+<section class="resumen">
+  <div class="row">
+    <div class="container">
+      <div class="col-md-4 cuadro">
+        <h4 class="text-center">Titulo</h4>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat obcaecati quibusdam, ex, laudantium, voluptatibus odit harum rem voluptate aut vel veritatis aliquam numquam modi consequuntur sed nesciunt molestias ratione.</p>
+      </div>
+       <div class="col-md-4 cuadro">
+        <h4 class="text-center">Titulo</h4>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat obcaecati quibusdam, ex, laudantium, voluptatibus odit harum rem voluptate aut vel veritatis aliquam numquam modi consequuntur sed nesciunt molestias ratione.</p>
+      </div>
+       <div class="col-md-4 cuadro">
+        <h4 class="text-center">Titulo</h4>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat obcaecati quibusdam, ex, laudantium, voluptatibus odit harum rem voluptate aut vel veritatis aliquam numquam modi consequuntur sed nesciunt molestias ratione.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<footer class="footer">
+    <div class="container">
+        <p class="text-center"> &copy; Jhassir Candia 2018</p>
+    </div>
+</footer>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
